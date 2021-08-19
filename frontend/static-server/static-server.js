2e1990ab55b51275d9572ae40e473eb15175741a
Anton Haubner <anton.haubner@outlook.de>
2021-08-19 23:51:38 +0200

    Disable caching of frontend for non-static content

--- frontend/static-server/static-server.js
+++ frontend/static-server/static-server.js
@@ -1,11 +1,27 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires,no-undef
 const express = require('express');
-// eslint-disable-next-line @typescript-eslint/no-var-requires,no-undef
 const compression = require('compression');
+const helmet = require('helmet');
 const app = express(); // create express app
 
 app.use(compression());
-app.use(express.static('build'));
+app.use(helmet());
+
+const servedDirPrefix = process.cwd() + '/build';
+const servedDirPrefixLength = servedDirPrefix.length;
+
+app.use(
+  express.static(servedDirPrefix, {
+    cacheControl: true,
+    // https://create-react-app.dev/docs/production-build/#static-file-caching
+    setHeaders: function (res, path) {
+      if (path.startsWith('/static', servedDirPrefixLength)) {
+        res.setHeader('Cache-Control', 'Cache-Control: max-age=31536000');
+      } else {
+        res.setHeader('Cache-Control', 'no-cache');
+      }
+    },
+  })
+);
 
 // start express server on port 5000
 app.listen(8000, () => {
